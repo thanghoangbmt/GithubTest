eb0d6dd381900562cbdb580b1c0d45a892f144f5
= <=>
2020-08-13 14:47:27 +0700

    Edit Animal class

--- src/thangha/dtos/Animal.java
+++ src/thangha/dtos/Animal.java
@@ -1,5 +1,29 @@
 package thangha.dtos;
 
 public class Animal {
+	private String name;
+	private int age;
+
+	public Animal(String name, int age) {
+		super();
+		this.name = name;
+		this.age = age;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
 
 }
